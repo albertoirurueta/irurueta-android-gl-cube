7bf320372c87973754260d81ffa139679ed2c266
Alberto Irurueta Carro <albertoicar@ext.inditex.com>
2025-07-29 09:48:34 +0200

    Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.irurueta.android.gl.cube.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1.3"
+        versionCode = 6
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
