5cc9650c03c831a18d3659bd50b29cea5a3a9264
Alberto Irurueta Carro <albertoicar@ext.inditex.com>
2026-03-06 12:06:32 +0100

    Release 1.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 6
-        versionName = "1.1.6"
+        versionName = "1.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
